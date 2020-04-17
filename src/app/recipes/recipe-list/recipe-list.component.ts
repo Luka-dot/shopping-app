3d73ca129f5daf4ce454f92e41f497a619993b19
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-17 18:16:10 -0400

    recipe display set-up, hardcoded recipes are displaying

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  // defining type. TS will expect Recipe class[] with recipes in it
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg'),
+    new Recipe('2nd Test Recipe', 'Just Another test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg')
+  ];
 
   constructor() { }
 
