05a9e4af2e07a359dba60a1080b1443bbadba052
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-21 16:30:04 -0400

    recipe ingredients displays

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from '../shared/ingredient.model';
 // defining single recipe for use later thru app. MODEL
 // TS public will allow to be accessed from anywhere
 
@@ -5,10 +6,12 @@ export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, description: string, imagePath: string) {
+    constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,21 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     // private will prevent to be accessible from outside. Only using it now to test and make sure what im accessing 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Just a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg'),
-        new Recipe('2nd Test Recipe', 'Just Another test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg')
+        new Recipe('A Test Recipe', 'Just a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg',
+        [
+            new Ingredient('Meat', 1)
+        ]),
+        new Recipe('2nd Test Recipe', 'Just Another test recipe', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/17th_Century_Food.jpg',
+        [
+            new Ingredient('Potatoes', 2),
+            new Ingredient('Mystery meat', 2)
+        ])
       ];
 
       getRecipes() {
